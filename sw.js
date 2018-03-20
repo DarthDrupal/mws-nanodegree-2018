1fc2b12de6f4db721677c935de819faa7636fa30
DarthDrupal <mirko.marraro85@gmail.com>
2018-03-20 17:43:14 +0100

    style: Indentation and external sources
    Fixed some indentation and added
    the external source where I got the
    function to cache all the requests
    in the service worker fetch event listener.

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
 var staticCacheName = 'mws-restarurants-v7';
 
+/**
+ * @description Listener to manage the newer version of the cache
+ */
 self.addEventListener('activate', function (event) {
   event.waitUntil(
     caches.keys().then(function (cacheNames) {
@@ -7,21 +10,25 @@ self.addEventListener('activate', function (event) {
         cacheNames.filter(function (cacheName) {
           return cacheName.startsWith('mws-restarurants-') &&
             cacheName != staticCacheName;
-          }).map(function (cacheName) {
-            return caches.delete(cacheName);
+        }).map(function (cacheName) {
+          return caches.delete(cacheName);
         })
       );
     })
   );
 });
 
+/**
+ * @description Offline first implementation to cache all the generated requests
+ * Source: https://jakearchibald.com/2014/offline-cookbook/#on-network-response
+ */
 self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.open(staticCacheName).then(function (cache) {
       return cache.match(event.request).then(function (response) {
         return response || fetch(event.request).then(function (response) {
           cache.put(event.request, response.clone());
-            return response;
+          return response;
         });
       });
     })
